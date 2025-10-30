6798ca25da89ac037be75f21dd476c6c6150b6c3
azaeeeiiii <aceangelab@gmail.com>
2025-10-30 08:56:32 +0800

    edit

--- User.java
+++ User.java
@@ -1,6 +1,7 @@
 public class User {
     public static void main(String[] args) {
         System.out.println("This is the User class.");
+        System.out.println("You can edit here.");
     }
     
 }
