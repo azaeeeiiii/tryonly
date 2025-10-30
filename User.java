b526361e63a4943801f83ea17876ae20f9295479
deynblip <danecura09@gmail.com>
2025-10-30 09:19:06 +0800

    message

--- User.java
+++ User.java
@@ -2,6 +2,11 @@ public class User {
     public static void main(String[] args) {
         System.out.println("This is the User class.");
         System.out.println("You can edit here.");
+        System.out.println("dane test");
+        System.out.println("dane test");
+        System.out.println("dane test");
+        System.out.println("dane test");
+        System.out.println("dane test");
     }
     
 }
