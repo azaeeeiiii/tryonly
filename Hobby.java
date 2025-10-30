3d8da859531011945af3d17e3c1a3570b9839693
azaeeeiiii <aceangelab@gmail.com>
2025-10-30 09:04:54 +0800

    hobby

---
+++ Hobby.java
@@ -0,0 +1,7 @@
+public class Hobby {
+    public static void main(String[] args) {
+        System.out.println("My hobby is painting.");
+        System.out.println("I love to create art in my free time.");
+    }
+    
+}
